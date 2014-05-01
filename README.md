7fb0de1cae7afe3984f983265ec90adfde249e3a
Nina Hwang <nihwang12@gmail.com>
2014-05-01 12:09:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+We are Team It's Nacho Problem
+- Nina Hwang
+- Paolo Payumo
+- Britt McGinnis
+- Erin Snyder
---
+++ source/its_nacho_problem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItsNachoProblem::Application.config.secret_token = 'f41f945b37f0d0abe3a17c2e5ab6af9a3383edb97d793a1d30b282b14963c4308cc4073431bc0cf2d24e0e58ddabe50980dc8fe567e1c11e2b3703d43739cf9b'
