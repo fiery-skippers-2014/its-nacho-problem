1a85a59ed0a222b549e2c1a18c69d78a14dd73ba
Paolo Payumo <paolo.payumo@yahoo.com>
2014-05-01 16:00:13 -0700

    all tests are passing; recipe model has validations; added git ignore file; added rspec file for pretty colors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ActiveRecord::Base
   serialize :components
   attr_accessible :components
+
+  validates_presence_of :name, :recipe_url, :components
+  validates_uniqueness_of :name, :recipe_url
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Recipe do
   it {should validate_presence_of(:name)}
-  it {should validate_presence_of(:ingredients)}
+  it {should validate_presence_of(:components)}
   it {should validate_presence_of(:recipe_url)}
   it {should validate_uniqueness_of(:name)}
   it {should validate_uniqueness_of(:recipe_url)}
