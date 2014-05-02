369455b99d75ae0361348ada8a50a466d6f51813
Paolo Payumo <paolo.payumo@yahoo.com>
2014-05-01 21:02:29 -0700

    switching branches

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20140501234553) do
-
 
   create_table "recipes", :force => true do |t|
     t.string "name"
