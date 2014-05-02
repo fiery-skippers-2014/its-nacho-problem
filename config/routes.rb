1da1e835fccebfce8686403c6ea98c82ad658c7d
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-05-01 17:04:53 -0700

    We have sessions and users! * 3 tests pass (2 controller tests and 1 feature test) * As a user, I can create an account with a secure password using BCrypt * As a user, I can login to my account

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 ItsNachoProblem::Application.routes.draw do
-  
+  resources :sessions, :users, only: [:new, :create]
+
+  get '/logout' => 'sessions#destroy', :as => 'logout'
+  root to: 'users#index'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20140501225103) do
+
+  create_table "users", :force => true do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
