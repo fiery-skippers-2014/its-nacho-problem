b162ef31fb0431677920beb43da19d13a53f71bf
Brittan McGinnis <konamax123@gmail.com>
2014-05-02 11:50:06 -0700

    playing with before_filter

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,27 +1,39 @@
 class RecipesController < ApplicationController
+  before_filter :create do |controller|
+    controller.instance_eval do 
+      yummly_search_result
+      fetch_name(list)
+      fetch_id(list)
+      fetch_url(list)
+    end
+  end
 
 
+  def create
+    new_recipe = Recipe.create(name: @recipe_name, image_url: @recipe_url, recipe_url: "http://www.yummly.com/recipes/", )
+  end
 
-  def yummly_search_result
+
+  def yummly_search_result('Bacon')
     result = Yummly.search('Bacon')
     recipe_list = result.matches
   end
 
   def fetch_name(list)
     recipe_names = list.map do |recipe|
-      recipe["recipeName"]
+      @recipe_name = recipe["recipeName"]
     end 
   end
 
   def fetch_id(list)
     recipe_id = list.map do |recipe|
-      recipe["id"]
+      @recipe_id = recipe["id"]
     end
   end
 
   def fetch_url(list)
     recipe_url = list.map do |recipe|
-      recipe["smallImageUrls"]
+      @recipe_url = recipe["smallImageUrls"]
     end
   end
 
