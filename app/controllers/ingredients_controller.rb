fb6a4a0ae3faff3bf7b67e45374747cbe3eca1ad
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-05-02 15:06:28 -0700

    Search bar works and returns results from the database

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -9,18 +9,7 @@ class IngredientsController < ApplicationController
   end
 
   def create
-    render :index
+    render :index   #TODO allow a user to add items to the ingredients database?
   end
 
 end
-
-
-
-
-  # def self.search(search)
-  #   if search
-  #     find(:all, :conditions => ['name LIKE ?', "%#{search}%"])   # Added wildcards to both sides of the search field, so that a search for cheese will also return 'creamcheese', 'cheesecake' and 'cheddar cheese'
-  #   else
-  #     find(:all)
-  #   end
-  # end
