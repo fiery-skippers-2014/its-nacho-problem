9863c8ab9b0fc5cc1a776e77e6fbcd35d8714f43
Nina Hwang <nihwang12@gmail.com>
2014-05-04 14:43:23 -0700

    now validation for a user to validate uniqueness of ingredients

--- app/models/user_ingredient.rb
+++ app/models/user_ingredient.rb
@@ -2,7 +2,7 @@ class UserIngredient<ActiveRecord::Base
   belongs_to :user
   belongs_to :ingredient  
   attr_accessible :ingredient_ids, :ingredient_id, :user_id
-  
+	validates_uniqueness_of :ingredient_id, :scope => :user_id  
  
   
 end
