fb622d2ac6518dd5a6787510ebea171c773169f4
Paolo Payumo <paolo.payumo@yahoo.com>
2014-05-04 19:56:44 -0700

    feature test for removing ingredient

---
+++ spec/features/remove_ingredient_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature "removing ingredient from pantry" do
+  let!(:ingredient) {Ingredient.create(name: 'apple')}
+  let!(:user) {FactoryGirl.create :user}
+
+  before(:each) do
+    page.set_rack_session(:user_id => user.id)
+    user.ingredients << ingredient
+    visit user_ingredients_path(user)
+  end
+
+  scenario "a user can add click on a checkbox for the ingreident", js: true do
+    click_on "x"
+    expect(page).not_to have_content('apple')
+  end
+end
