57066a5389ae99d6bdfdcabff06d9324d366bb15
Brittan McGinnis <konamax123@gmail.com>
2014-05-05 08:51:35 -0700

    added default image

--- db/migrate/20140505050234_add_default_image_to_recipe.rb
+++ db/migrate/20140505050234_add_default_image_to_recipe.rb
@@ -1,5 +1,5 @@
 class AddDefaultImageToRecipe < ActiveRecord::Migration
   def change
-    change_column :recipes, :img_url, :binary, :default => "images/nachoprob"  
+    change_column :recipes, :img_url, :string, :default => "http://imgur.com/YwmTBuJ"  
   end
 end
