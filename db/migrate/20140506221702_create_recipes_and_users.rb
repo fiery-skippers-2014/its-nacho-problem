57ee579fa7881dae6574135d10d12622b40fcfd4
Paolo Payumo <paolo.payumo@yahoo.com>
2014-05-07 10:15:41 -0700

    changed the favorites table and migration; you will have to DROP your database and remigrate; dont forget rake:db:test:prepare :); all tests passing; made a new test for favorites

--- db/migrate/20140506221702_create_recipes_and_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateRecipesAndUsers < ActiveRecord::Migration
-  def change
-    create_table :recipes_users do |col|
-      col.belongs_to :recipe
-      col.belongs_to :user
-    end
-  end
-end
