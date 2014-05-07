11fd5d6ee5ab67925686465d3ea4309e420b3190
Nina Hwang <nihwang12@gmail.com>
2014-05-06 19:12:05 -0700

    got pagination to work, now have 9 per page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,9 @@ class RecipesController < ApplicationController
 
   def show
     @user = current_user
-    @top_recipes = current_user.sort_recipes_by_ingredients(10) #=> hard coded, changes number of recipes
+    @top_recipes = current_user.sort_recipes_by_ingredients(45) #=> hard coded, changes number of recipes
     @recipe_objects = current_user.sort_recipes_by_percentage(@top_recipes)
-    @paginatable_array = Kaminari.paginate_array(@recipe_objects).page(params[:page]).per(3)
+    @paginatable_array = Kaminari.paginate_array(@recipe_objects).page(params[:page]).per(9)
   end
 
   def create
