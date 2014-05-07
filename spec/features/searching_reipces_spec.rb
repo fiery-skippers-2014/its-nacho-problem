db391cd6536699dd43bca89f9e433a21432b929b
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-05-07 11:58:30 -0700

    Moved recipes into a partial on recipes/show, and added a few tests to searching_recipes_spec

--- spec/features/searching_reipces_spec.rb
+++ spec/features/searching_reipces_spec.rb
@@ -8,16 +8,29 @@ feature 'searching for ingredient in serach bar' do
   before(:each) do
     page.set_rack_session(:user_id => user.id)
     user.ingredients << ingredient
-    visit user_ingredients_path(user)
   end
 
   scenario "a user will be shown recipes sorted by ingredients that they have" do
+    visit user_ingredients_path(user)
     click_on 'Nacho Problem'
     expect(page).to have_content('Apple Pie')
-  end  
+  end
 
   scenario "a user will be shown missing ingredients for each recipe" do
+    visit user_ingredients_path(user)
     click_on 'Nacho Problem'
     expect(page).to have_content('Missing Ingredients: pie')
+  end
+
+  scenario "a user can click on See more to toggle down the rest of the recipe" do
+    visit user_nachoresult_path(user)
+    click_on "See more"
+    expect(page).to have_content("See Recipe")
+  end
+
+  scenario "a user will be able to return to the pantry page from the Nacho page" do
+    visit user_nachoresult_path(user)
+    click_on "Back To Pantry"
+    expect(page).to have_content("#{user.username}'s Pantry")
   end
 end
