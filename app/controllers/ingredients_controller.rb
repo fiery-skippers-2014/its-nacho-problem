7792b5182cf8e739788ad9993e5f2ef1f0b7d65f
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-05-07 09:43:22 -0700

    blah

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,20 +1,15 @@
 class IngredientsController < ApplicationController
   before_filter :authenticate!
   def index
-    
   end
-
   def search
     @search = params[:search]
     @ingredients = Ingredient.search(@search)
     render partial: 'search_results', locals: {ingredients: @ingredients}
   end
-
   def new
   end
-
   def create
     @user = current_user
   end
-
 end
