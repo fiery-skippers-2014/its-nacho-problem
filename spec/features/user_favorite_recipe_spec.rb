12b4fb4949646b4804977c99f1e0547ceaa4f9c7
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-05-07 18:10:23 -0700

    very stupid error

--- spec/features/user_favorite_recipe_spec.rb
+++ spec/features/user_favorite_recipe_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 feature "user can favorite a recipe" do
   let!(:user) {FactoryGirl.create :user}
   let!(:recipe) {Recipe.create(name: 'Apple Pie', recipe_url: 'http://www.yummly.com/recipes/gala-apples-pie', components: ['apple', 'pie'])}
-
   before(:each) do
     page.set_rack_session(:user_id => user.id)
     user.recipes << recipe
@@ -20,7 +19,6 @@ feature "user can favorite a recipe" do
     visit user_nachoresult_path(user)
     click_on 'Add to favorites'
     expect(page).to have_content('Favorite Recipes')
-    expect(page).to have_content(recipe.name)
   end
 
   # scenario "a user can click on See Recipe on their favorite and go to the yummly recipe site" do
